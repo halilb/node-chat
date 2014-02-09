10491d933c8a120de9dff40d91c85ea5370e6b30
Halil Bilir <hbilir@gmail.com>
2014-02-09 23:13:04 +0200

    defined SockMock and started using it for jasmine tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
     karma: {
       unit: {
         configFile: 'karma.conf.js',
-        singleRun: false
+        singleRun: true
       }
     }
   });
--- test/jasmine/chat.spec.js
+++ test/jasmine/chat.spec.js
@@ -1,38 +1,75 @@
 'use strict';
 
+/*
+Simple mock for socket.io
+see: https://github.com/hackify/hackify-server/blob/master/test/controllers.test.js
+and https://github.com/btford/angular-socket-io-seed/issues/4
+*/
+function SockMock($rootScope){
+  this.events = {};
+  this.emits = {};
+
+  // intercept 'on' calls and capture the callbacks
+  this.on = function(eventName, callback){
+    if(!this.events[eventName]) this.events[eventName] = [];
+    this.events[eventName].push(callback);
+  };
+
+  // intercept 'emit' calls from the client and record them to assert against in the test
+  this.emit = function(eventName){
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    if(!this.emits[eventName])
+      this.emits[eventName] = [];
+    this.emits[eventName].push(args);
+  };
+
+  //simulate an inbound message to the socket from the server (only called from the test)
+  this.receive = function(eventName){
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    if(this.events[eventName]){
+      angular.forEach(this.events[eventName], function(callback){
+        $rootScope.$apply(function() {
+          callback.apply(this, args);
+        });
+      });
+    }
+  };
+
+}
+
 describe('ChatCtrl', function() {
-
-
     beforeEach(module('chat'));
-    beforeEach(module('btford.socket-io'));
 
-    var socket,
-        scope,
-        $timeout,
-        $browser,
-        mockIoSocket,
-        ctrl;
+    var scope,
+      socketMock;
 
-    beforeEach(inject(function (socketFactory, _$browser_, $rootScope, _$timeout_, $controller) {
-        $browser = _$browser_;
-        $timeout = _$timeout_;
-        scope = $rootScope.$new();
-        mockIoSocket = io.connect();
-        socket = socketFactory({
-          ioSocket: mockIoSocket,
-          scope: scope
-        });
-        ctrl = $controller('ChatCtrl', {
-            $scope: scope
-        });
+    beforeEach(angular.mock.inject(function($rootScope, $controller) {
+      scope = $rootScope.$new();
+
+      socketMock = new SockMock($rootScope);
+
+      $controller('ChatCtrl', {$scope: scope, socket: socketMock});
     }));
 
-
-    it('should say hello', function() {
-        // FIXME: socket is currently not working
-        mockIoSocket.on('nameResult', {name: 'Guest1'});
-        expect(ctrl.nickName).toBe('Guest1');
+    it('should set username correctly', function() {
+      socketMock.receive('nameResult', {'name': 'Guest1'});
+      expect(scope.nickName).toBe('Guest1');
     });
 
+    it('should get new messages', function() {
+      socketMock.receive('send:message', 'this is the new message');
+      expect(scope.messages[0]).toBe('this is the new message');
+    });
+
+    it('should send new message with nickName', function() {
+      scope.message = 'send this message';
+      scope.nickName = 'Guest1';
+      scope.sendMessage();
+
+      expect(scope.messages[0].author).toBe('Guest1');
+      expect(scope.messages[0].content).toBe('send this message');
+    });
 
 });
